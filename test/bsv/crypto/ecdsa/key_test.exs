5e9c4312467ad6d485d8cf26a9dfd1ecd5a1cf87
aaronrussell <aaron@gc4.co.uk>
2019-08-13 23:23:41 +0100

    ECDSA module for key generation, signing and verification, pem encoding.

---
+++ test/bsv/crypto/ecdsa/key_test.exs
@@ -0,0 +1,5 @@
+defmodule BSV.Crypto.ECDSA.KeyTest do
+  use ExUnit.Case
+  doctest BSV.Crypto.ECDSA.Key
+
+end
---
+++ test/bsv/crypto/ecdsa_test.exs
@@ -0,0 +1,5 @@
+defmodule BSV.Crypto.ECDSATest do
+  use ExUnit.Case
+  doctest BSV.Crypto.ECDSA
+
+end
