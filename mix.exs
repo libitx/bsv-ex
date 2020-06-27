80466b72189d3e391099cac65234405e002e8ab0
libitx <libitx@protonmail.com>
2020-06-27 16:46:34 +0100

    Minor version bump.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BSV.MixProject do
   def project do
     [
       app: :bsv,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
