d496d0bc6255f14fb65a080d7ef71090998ba897
libitx <libitx@protonmail.com>
2021-11-19 16:56:16 +0000

    Serializable docs.

--- lib/bsv/serializable.ex
+++ lib/bsv/serializable.ex
@@ -1,16 +1,16 @@
 defprotocol BSV.Serializable do
-  @moduledoc """
-  TODO
-  """
+  @moduledoc false
 
   @doc """
-  TODO
+  Parses the binary value into the specified type.
+
+  Returns a tuple containing the parsed term and the remaining binary data.
   """
   @spec parse(t(), binary()) :: {:ok, t(), binary()} | {:error, term()}
   def parse(type, data)
 
   @doc """
-  TODO
+  Parses the struct into a binary value.
   """
   @spec serialize(t()) :: binary()
   def serialize(type)
